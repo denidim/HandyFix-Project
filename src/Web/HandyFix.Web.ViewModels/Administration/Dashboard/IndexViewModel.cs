a75a57849a13f63cc1d2c923fa526d8e9131e39d
denidim <fs900220@gmail.com>
2026-07-08 01:53:51 +0100

    feat(dashboard): add database stat fields to IndexViewModel

--- src/Web/HandyFix.Web.ViewModels/Administration/Dashboard/IndexViewModel.cs
+++ src/Web/HandyFix.Web.ViewModels/Administration/Dashboard/IndexViewModel.cs
@@ -1,7 +1,17 @@
-﻿namespace HandyFix.Web.ViewModels.Administration.Dashboard
+namespace HandyFix.Web.ViewModels.Administration.Dashboard
 {
     public class IndexViewModel
     {
         public int SettingsCount { get; set; }
+
+        public int TotalBookingsCount { get; set; }
+
+        public int PendingBookingsCount { get; set; }
+
+        public int TotalEnquiriesCount { get; set; }
+
+        public int PendingReviewsCount { get; set; }
+
+        public decimal TotalRevenue { get; set; }
     }
 }
